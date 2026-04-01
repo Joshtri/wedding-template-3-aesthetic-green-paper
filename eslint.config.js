23da8f15f05eb2f7fa1695ac43bfbe22c1eb66f2
Joshtri <stuffofyos1516@gmail.com>
2026-04-01 22:57:09 +0700

    feat: add react-hooks linting rules to eslint configuration

--- eslint.config.js
+++ eslint.config.js
@@ -19,5 +19,10 @@ export default defineConfig([
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+    rules: {
+      "react-hooks/exhaustive-deps": "warn",
+      "react-hooks/rules-of-hooks": "error",
+      // "@typesript-eslint"
+    },
   },
 ])
